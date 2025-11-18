9d103a10ef44afdc961d01540ce6c50bf2836af6
Anniken <annikenje@gmail.com>
2025-11-18 21:46:30 +0100

    Add finished image/camera modal

--- components/ImageSelectionModal.tsx
+++ components/ImageSelectionModal.tsx
@@ -1,8 +1,9 @@
+import { Colors } from "@/styles/colors";
+import * as Styles from "@/styles/componentStyle";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import * as ImagePicker from "expo-image-picker";
 import { useRef } from "react";
-import { Button, Text, View, TouchableOpacity } from "react-native";
-import * as Styles from "@/styles/componentStyle";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 // Props ----------------------------------
 type ImageSelectionModalProps = {
@@ -40,37 +41,76 @@ export default function ImageSelectionModal({
       quality: 1,
     });
     if (!result.canceled) {
-        setImage(result.assets[0].uri);
+      setImage(result.assets[0].uri);
+      closeModal();
+    }
+  }
+
+  async function captureImage() {
+    if (cameraRef.current) {
+      const image = await cameraRef.current.takePictureAsync();
+      if (image) {
+        setImage(image.uri);
         closeModal();
+      }
     }
   }
 
-  async function captureImage(){
-    if(cameraRef.current){
-        const image = await cameraRef.current.takePictureAsync();
-        if(image){
-            setImage(image.uri)
-            closeModal();
-        }
-    }
-  }
-
-// Return ----------------------------------
+  // Return ----------------------------------
   return (
-    <View style={Styles.Styles.centerContainer}>
-      <CameraView style={{ flex: 1}}facing="back" ref={cameraRef} />
-      <View >
-        <TouchableOpacity onPress={() => closeModal()}>
-          <Text>Avbryt</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => captureImage()}>
-          <Text > Ta bilde</Text>
-        </TouchableOpacity>
-        <TouchableOpacity  onPress={() => pickImage()}>
-          <Text >Velg bilde</Text>
+    <View style={[Styles.Styles.centerContainer, { flex: 1 }]}>
+      <Text style={{color: Colors.mainColor, fontWeight: "bold", fontSize: 24}}>Velg et bilde eller ta et nytt</Text>
+      <CameraView
+        style={style.camera}
+        facing="back"
+        ref={cameraRef}
+      />
+      <View>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity
+            style={[
+              Styles.Styles.button,
+              { backgroundColor: Colors.mainColor },
+            ]}
+            onPress={() => captureImage()}
+          >
+            <Text style={style.text}>Ta bilde</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              Styles.Styles.button,
+              { backgroundColor: Colors.mainColor },
+            ]}
+            onPress={() => pickImage()}
+          >
+            <Text style={style.text}>Velg bilde</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={[Styles.Styles.button, { backgroundColor: Colors.gray }]}
+          onPress={() => closeModal()}
+        >
+          <Text style={style.text}>Avbryt</Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-
+  );
 }
+
+// Styles ----------------------------------
+
+const style = StyleSheet.create({
+  camera: {
+    width: "100%",
+    height: 300,
+    borderRadius: 8,
+    margin: 8,
+    borderWidth: 3,
+    borderColor: Colors.gray,
+  },
+  text: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
